9ceabb348a99e62ea4b54ddf73911d1dba86fb99
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2024-01-04 14:07:05 +0530

    new file added

--- exp.js
+++ exp.js
@@ -5,5 +5,9 @@ var app=exp()
 app.get('/',(req,res)=>{
     res.send("hi and welcome")
 })
+app.get('/home', (req, res)=>{
+    res.send({msg:"welcome"})
+ })
+
 app.listen(6000)
 console.log('running on port 6000')
