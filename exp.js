429c19221af6e528e6491ff0d9c7ef6562a5e115
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2024-01-04 15:01:57 +0530

    aaa

--- exp.js
+++ exp.js
@@ -1,8 +1,10 @@
 //express
+const { json } = require('body-parser')
 let exp=require('express')
 
 var app=exp() //for init
 var fs=require('fs')
+var url=require('url')
 
 app.get('/',(req,res)=>{
     res.send("hi and welcome")
@@ -18,6 +20,23 @@ app.get('/home', (req, res)=>{ //just viewing
        res.send(JSON.parse(data))
    });
  })
+ app.get('/filewrite',(req,res)=>{ //to add data in json file
+    var query = url.parse(req.url,true).query;
+    fs.readFile('data.json',function(err,data){
+        if(err)
+            res.send({msg:'invalid',code:err})
 
-app.listen(3002)
-console.log('running on port 3002')
+        var mydata=JSON.parse(data)
+        mydata.push(query)
+        var result=JSON.stringify(data)
+        fs.writeFile('data.json',result, function (err) { 
+            if (err)
+                console.log(err);
+            else
+                res.send("sucess")
+        });
+    })
+ })
+
+app.listen(3003)
+console.log('running on port 3003')
