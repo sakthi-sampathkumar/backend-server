3a49f0ea1c00525e4eb6490e6490614bd4a3a815
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2024-01-04 13:38:18 +0530

    backend topic

---
+++ index.js
@@ -0,0 +1,33 @@
+var app=require('http')
+var server= app.createServer(function(req,res){
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+    }
+    else
+        res.end('Invalid Request!');
+
+});
+
+server.listen(4000);
+console.log('web server is running on port 4000');
